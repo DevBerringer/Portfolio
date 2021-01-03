9602ad48363d9dae5e73aef7850ba5ebcf596fb9
DevBerringer <39319615+DevBerringer@users.noreply.github.com>
2021-01-03 04:00:36 -0600

    Add files via upload

---
+++ Portfolio 2/script.js
@@ -0,0 +1,20 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
