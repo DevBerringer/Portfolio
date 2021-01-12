965fda06a4f2f77c44354d2d09701385f4c2fc19
DevBerringer <blakeberringer@gmail.com>
2021-01-12 15:13:52 -0600

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -6,6 +6,20 @@ $(function () {
 });
 
 
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
+
+
 let slider = document.querySelector('.slider');
 let innerSlider = document.querySelector('.slider-inner');
 
