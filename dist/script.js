104d73b1c3285213823ac05fb9b5bfbe397969ac
DevBerringer <blakeberringer@gmail.com>
2021-01-12 15:10:06 -0600

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -6,18 +6,60 @@ $(function () {
 });
 
 
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+let slider = document.querySelector('.slider');
+let innerSlider = document.querySelector('.slider-inner');
 
-navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+
+let pressed = false;
+let startX;
+let x;
+
+slider.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = 'grabbing'
+    slider.style.cursor = -webkit-grab
+
 });
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    document.body.classList.remove('nav-open');
-  })
-})
+slider.addEventListener('mouseenter', () => {
+    slider.style.cursor = 'grab'
+});
+
+slider.addEventListener('mouseup', () => {
+    slider.style.cursor = 'grab'
+});
+
+slider.addEventListener('mouseleave', () => {
+    pressed = false;
+});
+
+window.addEventListener('mouseup', () => {
+    pressed = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`;
+
+    checkboundary()
+});
+
+function checkboundary() {
+    let outer = slider.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = '0px'
+    } else if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`
+    }
+}
+
 
 //typeing animation script
 
