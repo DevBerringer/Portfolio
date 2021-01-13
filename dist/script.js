e9070260cf94e1f97fa8312418e31ae933bda5de
DevBerringer <blakeberringer@gmail.com>
2021-01-12 18:58:00 -0600

    text fixed

--- dist/script.js
+++ dist/script.js
@@ -77,7 +77,7 @@ function checkboundary() {
 //typeing animation script
 
 var typed = new Typed(".typing", {
-  strings:["Front-end, Back-end", "Full-stack", "Developer" ],
+  strings:["Front-end, Back-end", "Full-stack Developer" ],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true
